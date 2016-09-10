0ba953fbfc7e85c14252baf1273740eb892c1b4b
Jefferson Stachelski <jeffhsta@riseup.net>
2016-09-10 19:55:02 -0300

    Adds Python 3.5 async code example

--- python35/asyncapp/app.py
+++ python35/asyncapp/app.py
@@ -1,2 +1,18 @@
+import asyncio
+
+
+async def print_number(number):
+    print(number)
+
+
 def start():
     print('asyncapp started!')
+    loop = asyncio.get_event_loop()
+
+    loop.run_until_complete(
+       asyncio.wait([
+           print_number(number)
+           for number in range(10)
+       ])
+    )
+    loop.close()
